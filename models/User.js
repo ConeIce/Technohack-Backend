4ea93b84e0f9d86d2e3b242a3c576c57feb3edd6
Akhil <akhillav@gmail.com>
2023-03-19 01:56:07 +0530

    schema change

--- models/User.js
+++ models/User.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 const User = mongoose.Schema({
-    username: {type: String, required : true},
-    roomNumber: {type: String, required : true}
-})
+  username: { type: String, required: true },
+  roomNumber: { type: String, required: true },
+  location: { type: String },
+  available: { type: Boolean },
+});
 
 export default mongoose.model("User", User);
